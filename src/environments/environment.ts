a5365782fc4008a9fa8a1d95b2585117be9269e3
2010jmatpon <jmatpon2010@g.educaand.es>
2023-12-13 10:54:25 +0100

    Primer Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBb-7RloEKzqnPbS31XGqzcDgCzQoljsYU",
+    authDomain: "goles-hlc.firebaseapp.com",
+    projectId: "goles-hlc",
+    storageBucket: "goles-hlc.appspot.com",
+    messagingSenderId: "966886838613",
+    appId: "1:966886838613:web:8548bf113168a2c74c5fdc"
+  }
 };
 
 /*
