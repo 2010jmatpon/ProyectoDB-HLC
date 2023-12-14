57ed9f542ad37b8a41444d0f49e4bf5bd722609f
2010jmatpon <jmatpon2010@g.educaand.es>
2023-12-14 12:20:47 +0100

    Commit

---
+++ src/app/gol.ts
@@ -0,0 +1,5 @@
+export interface Gol {
+    jugador: string;
+    numGoles: number;
+    partidos: string;
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Gol} from '../gol';
+import { FirestoreService } from '../firestore.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,59 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  golEditando = {} as Gol;
+  arrayColeccionGoles: any = [{
+    id: "",
+    gol: {} as Gol
+}];
+idGolSelec: string = "";
 
+  constructor(private firestoreService: FirestoreService) {
+    this.obtenerListaGoles();
+  }
+
+  clickBotonInsertar(){
+    this.firestoreService.insertar("goles", this.golEditando).then(() => {
+    console.log('Gol Marcado!');
+    this.golEditando= {} as Gol;
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
+  obtenerListaGoles(){
+    this.firestoreService.consultar("goles").subscribe((datosRecibidos) => {
+      this.arrayColeccionGoles = [];
+      datosRecibidos.forEach((datosGol) => {
+        this.arrayColeccionGoles.push({
+          id: datosGol.payload.doc.id,
+          gol: datosGol.payload.doc.data()
+
+        })
+      });
+    });
+  }
+  selecGol(idGol:string, golSelec:Gol){
+    this.golEditando = golSelec;
+    this.idGolSelec = idGol;
+  }
+
+
+  clickBotonBorrar(){
+    this.firestoreService.borrar("goles", this.idGolSelec).then(() => {
+    console.log('Gol Anulado!');
+    this.golEditando= {} as Gol;
+    this.idGolSelec = "";
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
+  clickBotonModificar() {
+    this.firestoreService.modificar("tareas", this.idGolSelec, this.golEditando).then(() => {
+      console.log('Registro editado');
+    }, (error) => {
+      console.error(error);
+    });
+  }
 }
